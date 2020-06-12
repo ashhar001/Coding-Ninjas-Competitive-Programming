539740a9b7c1c4b6025bdedf9e560edbc3ce215c
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-12 13:37:36 +0530

    Update Even&Odd_Indexes.cpp

--- Prerequisites/Even&Odd_Indexes.cpp
+++ Prerequisites/Even&Odd_Indexes.cpp
@@ -1,45 +1,50 @@
-Check Case
+Even and Odd Indexes
 Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
-Constraints :
-Input can be any character
-Sample Input 1 :
-v
-Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
-
-
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
+Given an array of integers, print two integer values:
+First, the sum of all numbers which are even as well as whose index are even.
+Second, the sum of all numbers which are odd as well as whose index are odd.
+Print the two integers space separated. (Arrays is 0-indexed)
+Input:
+Given an integer denoting the size of array.
+Next line will have a line containing ‘n’ space separated integers.
+Constraints:
+1<=n<=10^5
+1 <= Ai <= 10^6 
+Output:
+Two space separated integers denoting even and odd sums respectively.
+Sample Input:
+5
+2 3 5 1 4
+Sample Output:
+6 4
+	
+	
+/************************************************************************** SOLUTION ****************************************************************************************/
+	
+	
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
-    
-    if(a >='a' && a <='z'){
-        cout<<"0";
-    }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
-    }
-    else{
-        cout<<"-1";
-    }
+int main( int argc , char ** argv )
+{
+	ios_base::sync_with_stdio(false) ; 
+	cin.tie(NULL) ; 
+	
+	int n;
+	cin>> n;
+	int* arr = new int[n];
+	int sum1=0, sum2=0;
+	for (int i = 0; i < n; ++i)
+	{
+		int a;
+		cin>>a;
+		arr[i] = a;
+		if(i%2==0 && a%2==0){
+			sum1+=a;
+		}else if(i%2 != 0 && a%2!=0){
+			sum2+=a;
+		}
+	}
+	cout << sum1<<" "<<sum2 << '\n';
+	return 0 ; 
+
 }
