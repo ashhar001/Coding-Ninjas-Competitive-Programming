90dab60d9f02bada2fba6be0b7c26b948813ec7f
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-16 10:30:57 +0530

    Create SumOfArray.cpp

---
+++ Basics of Recursion/SumOfArray.cpp
@@ -0,0 +1,31 @@
+Sum of Array
+Send Feedback
+Given an array of length N, you need to find and return the sum of all elements of the array.
+Do this recursively.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Output Format :
+Sum
+Constraints :
+1 <= N <= 10^3
+Sample Input 1 :
+3
+9 8 9
+Sample Output 1 :
+26
+Sample Input 2 :
+3
+4 2 1
+Sample Output 2 :
+7    
+
+
+/********************************************************** SOLUTION ****************************************************************/
+
+
+int sum(int input[], int n) {
+    if (n <= 0) 
+        return 0; 
+    return (sum(input, n - 1) + input[n - 1]); 
+}
