1b7bec56fde818f4218cb659efb68d022283625c
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-17 07:08:54 +0530

    Update DifferentNames.cpp

--- Language Tools/DifferentNames.cpp
+++ Language Tools/DifferentNames.cpp
@@ -24,3 +24,43 @@ Sample Output:
 /************************************************** SOLUTION ***********************************************/
 
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main( int argc , char ** argv )
+{
+	ios_base::sync_with_stdio(false) ; 
+	cin.tie(NULL) ; 
+	
+	string names;
+	getline(cin, names);
+	
+
+	stringstream iss(names);
+
+	unordered_map<string, int> m1;
+	string temp;
+
+	while(iss>>temp){
+		m1[temp]++;
+	}
+
+	unordered_map<string, int> :: iterator it=m1.begin();
+	int count = 0;
+	for (it; it != m1.end(); ++it)
+	{
+		if(it->second>1){
+		cout << it->first<<" "<< it->second << '\n';
+		count++;
+		}
+	}
+	if (count == 0)
+	{	
+		cout << -1 << '\n';
+	}
+
+	return 0 ; 
+
+
+}
