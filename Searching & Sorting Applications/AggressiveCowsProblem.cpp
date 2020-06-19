bfa4d51f176a57a3e03290143d9b4f5a62743eec
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-19 07:37:16 +0530

    Update AggressiveCowsProblem.cpp

--- Searching & Sorting Applications/AggressiveCowsProblem.cpp
+++ Searching & Sorting Applications/AggressiveCowsProblem.cpp
@@ -1,45 +1,82 @@
-Check Case
+Aggressive Cows Problem
 Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
-Constraints :
-Input can be any character
-Sample Input 1 :
-v
-Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
+Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1,...,xN (0 <= xi <= 1,000,000,000).
+His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+Input
+t – the number of test cases, then t test cases follows. 
+* Line 1: Two space-separated integers: N and C
+* Lines 2..N+1: Line i+1 contains an integer stall location, xi
+Output
+For each test case output one integer: the largest minimum distance.
+Sample Input :
 1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
+5 3
+1
+2
+8
+4
+9
+Sample Output:
+3 
+Output details:
+FJ can put his 3 cows in the stalls at positions 1, 4 and 8, 
+resulting in a minimum distance of 3.
 
+	
 
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
+/********************************************************** SOLUTION *******************************************************/
+	
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
-    
-    if(a >='a' && a <='z'){
-        cout<<"0";
-    }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
-    }
-    else{
-        cout<<"-1";
-    }
+
+bool check(int cows,long long positions[],int n,long long distance){
+
+	int count = 1;
+	long long last_position = positions[0];
+
+	for(int i=1;i<n;i++){
+		if(positions[i] - last_position >= distance){
+			last_position = positions[i];
+			count++;
+		}
+
+		if(count == cows){
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n,c;
+		cin >> n >> c;
+
+		long long positions[n];
+		for(int i=0;i<n;i++){
+			cin >> positions[i];
+		}
+		sort(positions,positions+n);
+		long long start = 0;
+		long long end = positions[n-1] - positions[0];
+
+		long long ans = -1;
+
+		while(start<=end){
+			long long mid = start + (end-start)/2;
+
+			if(check(c,positions,n,mid)){
+				ans = mid;
+				start = mid+1;
+			}else{
+				end = mid-1;
+			}
+
+		}
+		cout << ans <<endl;
+	}
+
+	return 0;
 }
