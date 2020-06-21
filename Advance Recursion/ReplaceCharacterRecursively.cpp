6724f91cbe0faaed86133b9be167466b8674b8a1
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-21 20:47:18 +0530

    Update ReplaceCharacterRecursively.cpp

--- Advance Recursion/ReplaceCharacterRecursively.cpp
+++ Advance Recursion/ReplaceCharacterRecursively.cpp
@@ -1,45 +1,29 @@
-Check Case
+Replace Character Recursively
 Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
+Given an input string S and two characters c1 and c2, you need to replace every occurrence of character c1 with character c2 in the given string.
+Do this recursively.
+Input Format :
+Line 1 : Input String S
+Line 2 : Character c1 and c2 (separated by space)
+Output Format :
+Updated string
 Constraints :
-Input can be any character
-Sample Input 1 :
-v
-Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
+1 <= Length of String S <= 10^6
+Sample Input :
+abacd
+a x
+Sample Output :
+xbxcd
 
 
-/****************************************** SOLUTION *************************************************************************************/
+/**************************************** SOLUTION *************************************/
 
-#include<iostream>
-using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
-    
-    if(a >='a' && a <='z'){
-        cout<<"0";
+void replaceCharacter(char input[], char c1, char c2) {
+    if(input[0] == '\0') {
+        return;
     }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
+    if(input[0] == c1) {
+        input[0] = c2;
     }
-    else{
-        cout<<"-1";
-    }
+    replaceCharacter(input + 1, c1, c2);
 }
