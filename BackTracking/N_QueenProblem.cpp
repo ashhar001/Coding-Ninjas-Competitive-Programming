e81d5b33a44b03cb37dee64e9e7e175d591a1f9b
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-22 11:57:46 +0530

    Update N_QueenProblem.cpp

--- BackTracking/N_QueenProblem.cpp
+++ BackTracking/N_QueenProblem.cpp
@@ -1,45 +1,88 @@
-Check Case
+N-Queen Problem
 Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
+You are given N, and for a given N x N chessboard, find a way to place N queens such that no queen can attack any other queen on the chess board. A queen can be killed when it lies in the same row, or same column, or the same diagonal of any of the other queens. You have to print all such configurations.
+Input Format :
+Line 1 : Integer N
+Output Format :
+One Line for every board configuration. 
+Every line will have N*N board elements printed row wise and are separated by space
+Note : Don't print anything if there isn't any valid configuration.
 Constraints :
-Input can be any character
-Sample Input 1 :
-v
+1<=N<=10
+Sample Input 1:
+4
 Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
+0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 0 
+0 0 1 0 1 0 0 0 0 0 0 1 0 1 0 0 
+	
+	
+	
+	
+/******************************************* SOLUTION **************************************************/
 
+	
+	
+#include <bits/stdc++.h>
 
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
 using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
-    
-    if(a >='a' && a <='z'){
-        cout<<"0";
+
+int board[11][11];
+
+bool isPossible(int n,int row,int col){
+
+// Same Column
+  for(int i=row-1;i>=0;i--){
+    if(board[i][col] == 1){
+      return false;
     }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
+  }
+//Upper Left Diagonal
+  for(int i=row-1,j=col-1;i>=0 && j>=0 ; i--,j--){
+    if(board[i][j] ==1){
+      return false;
     }
-    else{
-        cout<<"-1";
+  }
+
+  // Upper Right Diagonal
+
+  for(int i=row-1,j=col+1;i>=0 && j<n ; i--,j++){
+    if(board[i][j] == 1){
+      return false;
     }
+  }
+
+  return true;
+}
+void nQueenHelper(int n,int row){
+  if(row==n){
+    // We have reached some solution.
+    // Print the board matrix
+    // return
+
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        cout << board[i][j] << " ";
+      }
+    }
+    cout<<endl;
+    return;
+
+  }
+
+  // Place at all possible positions and move to smaller problem
+  for(int j=0;j<n;j++){
+
+    if(isPossible(n,row,j)){
+      board[row][j] = 1;
+      nQueenHelper(n,row+1);
+      board[row][j] = 0;
+    }
+  }
+  return;
+
+}
+void placeNQueens(int n){
+    memset(board,0,11*11*sizeof(int));
+
+    nQueenHelper(n,0);
 }
