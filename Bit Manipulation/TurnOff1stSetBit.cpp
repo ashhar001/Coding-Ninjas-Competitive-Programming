d94cefa593c2dd1e27c6784e48efce5e7b5064fe
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-26 20:20:51 +0530

    Create TurnOff1stSetBit.cpp

---
+++ Bit Manipulation/TurnOff1stSetBit.cpp
@@ -0,0 +1,24 @@
+Turn off 1st set bit
+Send Feedback
+You are given an integer Ni. You need to make first set bit of binary representation of N to 0 and return the updated N.
+Counting of bits start from 0 from right to left.
+Input Format :
+Integer N 
+Output Format :
+Updated N
+Sample Input 1 :
+4
+Sample Output 1 :
+0
+Sample Input 2 :
+12 
+Sample Output 2 :
+8
+
+
+/******************************************************* SOLUTION ****************************************/
+
+int turnOffFirstSetBit(int n){
+   
+  return n & (n - 1);  
+}
