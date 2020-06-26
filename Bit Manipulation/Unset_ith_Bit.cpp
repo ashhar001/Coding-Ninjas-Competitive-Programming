494fc63bef75bd95824a70013b28ad651d3b66ca
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-06-26 20:18:17 +0530

    Create Unset_ith_Bit.cpp

---
+++ Bit Manipulation/Unset_ith_Bit.cpp
@@ -0,0 +1,32 @@
+Unset ith bit
+Send Feedback
+You are given two integers N and i. You need to make ith bit of binary representation of N to 0 and return the updated N.
+Counting of bits start from 0 from right to left.
+Input Format :
+Two integers N and i (separated by space)
+Output Format :
+Updated N
+Sample Input 1 :
+7 2
+Sample Output 1 :
+3
+Sample Input 2 :
+12 1
+Sample Output 2 :
+12
+
+
+/******************************************************* SOLUTION ****************************************/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int turnOffIthBit(int n, int i){
+    // k must be greater than 0 
+    if (i < 0) return n; 
+  
+    // Do & of n with a number with all set bits except 
+    // the k'th bit 
+    return (n & ~(1 << (i))); 
+}
+
