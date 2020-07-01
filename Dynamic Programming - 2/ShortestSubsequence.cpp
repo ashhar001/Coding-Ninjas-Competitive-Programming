92c4af89d42744e721fc23df7a0eb45d48c37b6a
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-07-01 11:45:14 +0530

    Create ShortestSubsequence.cpp

---
+++ Dynamic Programming - 2/ShortestSubsequence.cpp
@@ -0,0 +1,71 @@
+Shortest Subsequence
+Send Feedback
+Gary has two string S and V. Now Gary wants to know the length shortest subsequence in S such that it is not a subsequence in V.
+Note: input data will be such so there will always be a solution.
+Input Format :
+Line 1 : String S of length N (1 <= N <= 1000)
+Line 2 : String V of length M (1 <= M <= 1000)
+Output Format :
+Length of shortest subsequence in S such that it is not a subsequence in V
+Sample Input :
+babab
+babba
+Sample Output :
+3
+
+
+/******************************************************************** SOLUTION ***************************************************************************/
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int solve(string s1,string s2)
+{
+	int m =s1.size();
+	int n =s2.size();
+
+	vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+	for (int i = 0; i < n+1; ++i)
+	{
+		dp[m][i] = n-i;
+	}
+
+	for (int i = 0; i < m+1; ++i)
+	{
+		dp[i][n] = m-i;
+	}
+
+	for (int i = m-1; i >= 0; --i)
+	{
+		for (int j = n-1; j >= 0; --j)
+		{
+			int k = j;
+			
+			while(k<s2.size())
+			{
+				if(s2[k]==s1[i])
+				{
+					break;
+				}
+				k++;
+			}
+			if (k==s2.size())
+			{
+				dp[i][j] = 1;
+				//return 1;
+			}
+
+			int c1 = 1+dp[i+1][k+1];
+			int c2 = dp[i+1][j];
+
+			dp[i][j] = min(c1, c2);
+		}
+	}
+
+	return dp[0][0];
+
+
+}
