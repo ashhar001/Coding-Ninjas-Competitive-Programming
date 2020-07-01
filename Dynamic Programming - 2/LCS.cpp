71d9518cbdfdfb0c2ee7ec36dc82fe7fdaa2a481
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-07-01 11:08:50 +0530

    Update LCS.cpp

--- Dynamic Programming - 2/LCS.cpp
+++ Dynamic Programming - 2/LCS.cpp
@@ -1,45 +1,49 @@
-Check Case
+LCS - Problem
 Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
-Input format :
-Single Character
-Output format :
-1 or 0 or -1
-Constraints :
-Input can be any character
-Sample Input 1 :
-v
-Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
--1
-
-
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
-using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
-    
-    if(a >='a' && a <='z'){
-        cout<<"0";
+Given 2 strings of S1 and S2 with lengths m and n respectively, find the length of longest common subsequence.
+A subsequence of a string S whose length is n, is a string containing characters in same relative order as they are present in S, but not necessarily contiguous. Subsequences contain all the strings of length varying from 0 to n. E.g. subsequences of string "abc" are - "",a,b,c,ab,bc,ac,abc.
+Input Format :
+Line 1 : String S1
+Line 2 : String s2
+Output Format :
+Line 1 : Length of lcs
+Sample Input :
+adebc
+dcadb
+Sample Output :
+3
+	
+	
+/******************************************************************** SOLUTION ***************************************************************************/
+	
+	
+#include<iostream> 
+#include<cstring>
+using namespace std; 
+int lcs(string s, string t){
+    int m = s.size();
+    int n = t.size();
+    int **ans = new int*[m+1];
+    for(int i = 0; i <= m; i++) { 
+        ans[i] = new int[n+1];
     }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
+    // First row 
+    for(int j = 0; j <= n; j++) {
+        ans[0][j] = 0;
     }
-    else{
-        cout<<"-1";
+    // First col 
+    for(int i = 0; i <= m; i++) {
+        ans[i][0] = 0;
     }
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(s[m-i] == t[n-j]) {
+                ans[i][j] = 1 + ans[i-1][j-1];
+            }
+            else {
+                ans[i][j] = max(ans[i-1][j], ans[i][j-1]); 
+            }
+        }
+    }
+    return ans[m][n];
 }
