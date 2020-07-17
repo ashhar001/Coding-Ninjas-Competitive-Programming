c5bddf29e3a3adf0a2b57c1d9f3bc7a5921bba41
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-07-17 12:50:51 +0530

    Update StringSearch.cpp

--- String Algorithms/StringSearch.cpp
+++ String Algorithms/StringSearch.cpp
@@ -1,45 +1,104 @@
-Check Case
+String Search
 Send Feedback
-Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
-1, if the character is an uppercase alphabet (A - Z)
-0, if the character is a lowercase alphabet (a - z)
--1, if the character is not an alphabet
+Given two strings S and T, write a function to find if T is present as a substring inside S or not. If yes, return the starting index otherwise return -1.
 Input format :
-Single Character
-Output format :
-1 or 0 or -1
-Constraints :
-Input can be any character
-Sample Input 1 :
-v
-Sample Output 1 :
-0
-Sample Input 2 :
-V
-Sample Output 2 :
-1
-Sample Input 3 :
-#
-Sample Output 3 :
+
+Line 1 : String S
+
+Line 2 : String T
+
+Sample Input 1:
+WelcomeBack
+come 
+Sample Output 1:
+3
+Sample Input 2:
+WelcomeBack
+code
+Sample Output 2:
 -1
+	
+	
+/*********************************** SOLUTION ********************************************/
 
-
-/****************************************** SOLUTION *************************************************************************************/
-
-#include<iostream>
+	
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-	// Write your code here
-	char a;
-    cin>> a;
-    
-    if(a >='a' && a <='z'){
-        cout<<"0";
-    }
-    else if(a >= 'A' && a <= 'Z'){
-        cout<<"1";
-    }
-    else{
-        cout<<"-1";
-    }
+typedef long long ll;
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<int, bool> umapib;
+typedef unordered_map<string, int> umapsi;
+typedef unordered_map<string, string> umapss;
+typedef map<string, int> mapsi;
+typedef map<pair<int, int>, int> mappiii;
+typedef map<int, int> mapii;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef unordered_set<int> useti;
+
+#define uset unordered_set
+#define it iterator
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+#define MOD 1000000007
+
+
+int findString(char S[], char T[]) {
+	int length = strlen(T);
+	int n = strlen(S);
+
+
+	vector<int> pitable(length, 0);
+	int i = 0;
+	int j = i+1;
+
+	while(j<length && i<length){
+		if (T[i]==T[j])
+		{
+			pitable[j] = i+1;
+			i++;
+			j++;
+		}else
+		{
+			if (i!=0)
+			{
+				i = pitable[i-1];
+
+			}
+			j++;
+		}
+	}
+
+	
+
+	int p = 0;
+	int q = 0;
+	while(q<length && p<n){
+		if (S[p] == T[q])
+		{
+			p++;
+			q++;
+		}else
+		{
+			
+			if (q==0)
+			{
+				p++;
+				continue;
+			}
+			q = pitable[q-1];
+		}
+	}
+
+	if (q==length)
+	{
+		return p-length;
+	}
+
+
+	return -1;
+
 }
